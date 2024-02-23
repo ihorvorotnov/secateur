00fdd2560e234ea4230f9a7fe6fc820be4c2f749
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2024-02-23 22:12:50 +0100

    Check if in git repository

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Hello, I\'m Secateur!');
+import process from 'node:process';
+import {$} from 'execa';
+import chalk from 'chalk';
+import enquirer from 'enquirer';
+
+await $`git rev-parse --is-inside-work-tree`
+	.catch(error => {
+		console.error(chalk.red('Oopsie! This does not look like a git repository.'));
+		process.exit(1);
+	});
