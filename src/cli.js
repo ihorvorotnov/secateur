86df12b12a6f02af4934505f866c21f1e75f7141
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2024-03-23 16:46:07 +0100

    Wrap up initial version

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,7 @@ import process from 'node:process';
 import {$} from 'execa';
 import chalk from 'chalk';
 import enquirer from 'enquirer';
+import ansi from 'ansi-colors';
 
 await $`git rev-parse --is-inside-work-tree`
 	.catch(() => {
@@ -11,33 +12,68 @@ await $`git rev-parse --is-inside-work-tree`
 		process.exit(1);
 	});
 
-const branches = await $`git branch -vv --format %(HEAD)~%(refname:lstrip=2)~%(upstream:track)~%(upstream:lstrip=3)`;
+console.log();
 
-const branchesData = branches.stdout
-	.split('\n')
-	.map(branch => {
-		const [head, name, track, remoteName] = branch.split('~');
-		const disabledMap = {
-			'develop': 'Default',
-			'*': 'Current'
-		};
+const {Toggle, MultiSelect} = enquirer;
 
-		return {
-			name,
-			hint: track === '[gone]' ? 'Merged' : '',
-			disabled: disabledMap[name] || disabledMap[head] || false,
-		};
-	});
+new Toggle({
+	message: 'Show all branches?',
+	enabled: 'Yes',
+	disabled: 'No, only merged',
+})
+	.run()
+	.then(async (showAll) => {
 
-const {MultiSelect} = enquirer;
+		const branches = await $`git branch -vv --format %(HEAD)~%(refname:lstrip=2)~%(upstream:track)~%(upstream:lstrip=3)`;
 
-const prompt = new MultiSelect({
-	message: 'Select branches to delete:',
-	limit: 1000,
-	name: 'value',
-	choices: branchesData,
-});
+		const branchesData = branches.stdout
+			.split('\n')
+			.filter(branch => showAll ? showAll : branch.includes('[gone]'))
+			.map(branch => {
 
-prompt.run()
-	.then(answer => console.log('Answer:', answer))
+				const [head, name, track] = branch.split('~');
+				const disabledMap = {
+					'develop': chalk.red( 'DEFAULT' ),
+					'*': chalk.yellow( 'CURRENT' ),
+				};
+
+				return {
+					name,
+					hint: track === '[gone]' && showAll ? chalk.green( 'MERGED' ) : '',
+					disabled: disabledMap[name] || disabledMap[head] || false,
+				};
+			});
+
+			new MultiSelect({
+			message: 'Select branches to delete:',
+			limit: 1000,
+			name: 'value',
+			choices: branchesData,
+			indicator(state, choice) {
+				return choice.enabled
+					? chalk.white.bgGreen(' ' + state.symbols.indicator + ' ')
+					: chalk.gray.dim(' ' + state.symbols.indicator + ' ');
+			}
+		}).run()
+			.then(answer => {
+
+				console.log();
+
+				if (answer.length === 0) {
+					console.log(chalk.yellow('No branches selected.'));
+					process.exit(0);
+				}
+
+				answer.forEach(async branch => {
+					await $`git branch -D ${branch}`;
+				});
+
+				const word = answer.length === 1 ? 'branch' : 'branches';
+
+				console.log(chalk.green(`${ansi.symbols.check} Deleted ${answer.length} ${word}.`));
+
+			})
+			.catch(console.error);
+
+	})
 	.catch(console.error);
